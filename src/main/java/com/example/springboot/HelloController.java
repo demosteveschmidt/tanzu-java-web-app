b334c115aebb534cbeb3a2e6b7609bfe2ba7dd7c
Stefan Schmidt <sschmidt@pivotal.io>
2023-06-16 17:05:38 +0200

    updated /host message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
 	@RequestMapping("/host")
 	public String hostname() {
-		String message = "Running on: " + System.getenv("HOSTNAME") + "\n";
+		String message = "Running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
 		return message;
 	}
 
