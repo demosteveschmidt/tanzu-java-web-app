ec651ca25466fee78e9d2ce454c9468713fa5a84
Stefan Schmidt <sschmidt@pivotal.io>
2023-06-16 13:37:38 +0200

    typo

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,7 +21,7 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu! U1", controller.greeting());
+        assertEquals("Greetings from Spring Boot + Tanzu!", controller.greeting());
 
         mockMvc
             .perform(get("/"))
