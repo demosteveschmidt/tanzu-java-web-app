8dca64540a06e0a01e63f30e222b974fc15df8e9
Stefan Schmidt <sschmidt@pivotal.io>
2023-06-19 11:33:29 +0200

    gen3

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
 	@RequestMapping("/host")
 	public String hostname() {
-		String message = "Generation 2 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
+		String message = "Generation 3 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
 		return message;
 	}
 
