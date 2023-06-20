2bcec522d8762d876960633d74b255ee7787b9f9
Stefan Schmidt <sschmidt@pivotal.io>
2023-06-20 10:54:17 +0200

    removed istio from dev-steve, added to dev-lena

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
 	@RequestMapping("/host")
 	public String hostname() {
-		String message = "Generation 11 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
+		String message = "Generation 12 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
 		return message;
 	}
 
