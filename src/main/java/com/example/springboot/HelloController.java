8cac84d306414a1df3549100f1653421ffc8c245
Stefan Schmidt <sschmidt@pivotal.io>
2023-06-20 10:26:22 +0200

    gen10 - istio ingressgateway now has an LB IP

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
 	@RequestMapping("/host")
 	public String hostname() {
-		String message = "Generation 9 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
+		String message = "Generation 10 running on: " + System.getenv("HOSTNAME") + "\n            (HOSTNAME)";
 		return message;
 	}
 
